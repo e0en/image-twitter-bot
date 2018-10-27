546559195bbb118c3b7a36cd6bd366c9f539e054
Yoonseop Kang <e0engoon@gmail.com>
2018-10-27 07:30:31 +0000

    Resize large images before uploading

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # image-twitter-bot
+
 Upload images in folders with pre-defined captions to twitter
+
+## usage
+
+To resize large gifs, you need to install imagemagick to your system.
+
+```bash
+$ sudo apt install imagemagick  # ubuntu or debian
+$ sudo yum install imagemagick  # CentOS or redhat
+$ brew install imagemagick  # macos with homebrew
+```
+
+Then install requirements using the bundled `requirements.py`.
+
+```bash
+$ pip install -r requirements.txt
+```
--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
+import math
 from pathlib import Path
 import random
 import time
+import traceback
 import os
 
 import yaml
 import twitter
 import dropbox
+from wand.image import Image
 
 
 PWD = Path(__file__).parents[0].resolve()
@@ -25,6 +28,9 @@ for folder_to_caption in SETTING['folder_captions']:
     DIR_TO_TXT.update(**folder_to_caption)
 
 
+TWITTER_UPLOAD_LIMIT_BYTES = 5242880
+
+
 def write_log(msg):
     with open(LOG_FILE, 'a') as fp:
         ts = time.time()
@@ -35,6 +41,24 @@ def list_dropbox_files(dirname):
     files = [x.path_display for x in dbx.files_list_folder(dirname).entries
              if type(x) != dropbox.files.FolderMetadata]
     return files
+
+
+def resize_file(filename):
+    file_size = os.stat(tmp_filename).st_size
+    if file_size < TWITTER_UPLOAD_LIMIT_BYTES:
+        return filename
+
+    resize_factor = math.sqrt(TWITTER_UPLOAD_LIMIT_BYTES / float(file_size))
+    resize_factor *= 0.8
+    with Image(filename=filename) as img:
+        width = int(img.width * resize_factor)
+        height = int(img.height * resize_factor)
+
+        img.resize(width, height)
+        body, extension = filename.rsplit('.', 1)
+        new_filename = body + '_resized.' + extension
+        img.save(filename=new_filename)
+        return new_filename
 
 
 if __name__ == '__main__':
@@ -63,17 +87,26 @@ if __name__ == '__main__':
         write_log(f'Failed to download {filename} from dropbox')
         exit(1)
 
-    tmp_filename = PWD.joinpath(filename.split('/')[-1])
+    tmp_filename = str(PWD.joinpath(filename.split('/')[-1]))
     with open(tmp_filename, 'wb') as fp:
         fp.write(res.content)
 
-    with open(tmp_filename, 'rb') as fp:
-        write_log(f'Posting {filename} with caption {caption}')
-        twitter_api.PostUpdate(caption, media=fp)
-        write_log('Posting complete')
+    new_tmp_filename = resize_file(tmp_filename)
+    if new_tmp_filename != tmp_filename:
+        write_log(f'{tmp_filename} resized to {new_tmp_filename}')
+        os.remove(tmp_filename)
+        tmp_filename = new_tmp_filename
 
-    with open(PWD.joinpath('used_files.txt'), 'a') as fp:
-        fp.write(filename + '\n')
-        write_log('Used image list update complete')
+    with open(tmp_filename, 'rb') as fp:
+        try:
+            write_log(f'Posting {filename} with caption {caption}')
+            twitter_api.PostUpdate(caption, media=fp)
+            write_log('Posting complete')
+
+            with open(PWD.joinpath('used_files.txt'), 'a') as fp:
+                fp.write(filename + '\n')
+                write_log('Used image list update complete')
+        except:
+            write_log(traceback.format_exc())
 
     os.remove(tmp_filename)
